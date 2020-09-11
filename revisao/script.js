c41a21a0f640f7b9569eec1feab2e8428d236c85
Samuel <sampaioxsamuel@gmail.com>
2020-09-11 18:14:36 -0300

    clean dir

--- revisao/script.js
+++
@@ -1,6 +0,0 @@
-const dataHora = document.querySelector('[data-hora]')
-const funcionamento = document.querySelector('[data-semana]')
-
-const diaSemana = funcionamento.dataset.semana;
-
-console.log(diaSemana)
