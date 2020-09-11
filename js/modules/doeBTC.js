4c581fb12ccfe6c8ab9591cfdd3bbb57c3f8902f
Samuel <sampaioxsamuel@gmail.com>
2020-09-11 18:08:38 -0300

    ignore js

--- js/modules/doeBTC.js
+++
@@ -1,9 +0,0 @@
-export default function doeBTC() {
-  fetch('https://blockchain.info/ticker')
-    .then((r) => r.json())
-    .then((value) => {
-      const spanBTC = document.querySelector('.btc-preco');
-      const valor = (1000 / value.BRL.buy).toFixed(4);
-      spanBTC.innerText = `${valor}`;
-    });
-}
