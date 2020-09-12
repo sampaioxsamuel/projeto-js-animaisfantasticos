352492738535444ad5ee23d51da4b4033d3e7c7a
Samuel <sampaioxsamuel@gmail.com>
2020-09-12 17:24:42 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
@@ -14,9 +14,11 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 darkMode();
 initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initToolTip();
